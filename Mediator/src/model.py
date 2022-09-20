a21952bbbb0f42a96745cb564f02cd50620ac425
Matin Shokri <71878557+shokri-matin@users.noreply.github.com>
2022-09-20 18:28:50 +0430

    Add files via upload

---
+++ Mediator/src/model.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+
+class Model:
+
+    def __init__(self) -> None:
+
+        self.optimizer = tf.keras.optimizers.Adam()
+        self.loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+        self.model_base,  self.model_head = self.create_model()
+
+    def create_model(self):
+
+        head_size = 8
+        num_class = 2
+        dropout_rate = .3
+        
+        model_base = tf.keras.models.load_model("../Model/model_base.h5")
+
+        model_head = tf.keras.models.Sequential([
+                        tf.keras.layers.InputLayer(input_shape=(1024)),
+                        tf.keras.layers.Dense(head_size, kernel_regularizer=tf.keras.regularizers.L1(0.001)),
+                        tf.keras.layers.BatchNormalization(),
+                        tf.keras.layers.Activation('relu'),
+                        tf.keras.layers.Dropout(dropout_rate),
+                        tf.keras.layers.Dense(num_class, activation=tf.nn.softmax)
+                    ])
+        
+        model_head.compile(optimizer=self.optimizer , loss=self.loss_fn, metrics=['accuracy'])
+
+        return model_base, model_head
+
+    def get_weights(self):
+        weights = self.model_head.get_weights()
+        return weights
+
+    def set_weights(self, weights):
+        self.model_head.set_weights(weights)
+
+    def backpropagate(self, grads):
+        pass
+
+    def feedforward(self, x):
+        x_i = self.model_base(x, training=True)
+        return self.model_head(x_i, training=True)
+
+    def evaluate(self, x, y):
+        x_i = self.model_base(x, training=True)
+        test_loss, test_acc = self.model_head.evaluate(x_i,  y, verbose=2)
+        return test_loss, test_acc
